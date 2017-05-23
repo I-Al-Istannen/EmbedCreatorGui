9f8b4b4c2598831e8ed312823c1a6f4cbf0592cc
xaanit <shadowjacob@yahoo.com>
2017-05-22 23:55:34 -0500

    Auto variable registration

--- src/main/java/me/ialistannen/embedcreator/controller/MainScreenController.java
+++ src/main/java/me/ialistannen/embedcreator/controller/MainScreenController.java
@@ -1,8 +1,5 @@
 package me.ialistannen.embedcreator.controller;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -16,158 +13,167 @@ import me.ialistannen.embedcreator.model.CharacterLimit;
 import me.ialistannen.embedcreator.model.Variable;
 import me.ialistannen.embedcreator.model.VariableRegistry;
 import me.ialistannen.embedcreator.util.ImageUtil;
-import me.ialistannen.embedcreator.view.ControlPanel;
-import me.ialistannen.embedcreator.view.EditableTextFlow;
-import me.ialistannen.embedcreator.view.FieldContainer;
-import me.ialistannen.embedcreator.view.UrlImageView;
-import me.ialistannen.embedcreator.view.UrlLabel;
+import me.ialistannen.embedcreator.util.Util;
+import me.ialistannen.embedcreator.view.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A controller for the Main scene
  */
 public class MainScreenController {
 
-  @FXML
-  public ControlPanel controlPanel;
+    @FXML
+    public ControlPanel controlPanel;
 
-  @FXML
-  public GridPane rootPane;
+    @FXML
+    public GridPane rootPane;
 
-  @FXML
-  private UrlImageView thumbnailImage;
+    @FXML
+    private UrlImageView thumbnailImage;
 
-  @FXML
-  private UrlImageView authorAvatarImage;
+    @FXML
+    private UrlImageView authorAvatarImage;
 
-  @FXML
-  private UrlLabel authorName;
+    @FXML
+    private UrlLabel authorName;
 
-  @FXML
-  private UrlLabel title;
+    @FXML
+    private UrlLabel title;
 
-  @FXML
-  private EditableTextFlow descriptionText;
+    @FXML
+    private EditableTextFlow descriptionText;
 
-  @FXML
-  private FieldContainer fieldContainer;
+    @FXML
+    private FieldContainer fieldContainer;
 
-  @FXML
-  private UrlImageView image;
+    @FXML
+    private UrlImageView image;
 
-  @FXML
-  private UrlImageView footerImage;
+    @FXML
+    private UrlImageView footerImage;
 
-  @FXML
-  private EditableTextFlow footerText;
+    @FXML
+    private EditableTextFlow footerText;
 
-  @FXML
-  private void initialize() {
-    setClips();
+    @FXML
+    private void initialize() {
+        setClips();
 
-    VariableRegistry.addVariable(new Variable("Test", 20));
-//    initFields();
+        try {
+            for (Variable var : Util.getVariables()) {
+                System.out.println("Registering " + var.toString());
+                VariableRegistry.addVariable(var);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        // initFields();
 
-    ImageUtil.makeEditable(authorAvatarImage);
-    ImageUtil.makeEditable(thumbnailImage);
-    ImageUtil.makeEditable(image);
-    ImageUtil.makeEditable(footerImage);
+        ImageUtil.makeEditable(authorAvatarImage);
+        ImageUtil.makeEditable(thumbnailImage);
+        ImageUtil.makeEditable(image);
+        ImageUtil.makeEditable(footerImage);
 
-    controlPanel.setMainScreenController(this);
+        controlPanel.setMainScreenController(this);
 
-    descriptionText.getChildren().clear();
-    addText("Welcome", false);
-    addText(" {player} ", true);
-    addText("to the server!", false);
+        descriptionText.getChildren().clear();
+        addText("Welcome", false);
+        addText(" {player} ", true);
+        addText("to the server!", false);
 
-    descriptionText.setCharacterLimit(CharacterLimit.DESCRIPTION);
-    footerText.setCharacterLimit(CharacterLimit.FOOTER);
-    authorName.setCharacterLimit(CharacterLimit.AUTHOR);
-    title.setCharacterLimit(CharacterLimit.TITLE);
-  }
-
-  private void addText(String text, boolean variable) {
-    Text textNode = new Text(text);
-    if (variable) {
-      textNode.getStyleClass().add("variable");
+        descriptionText.setCharacterLimit(CharacterLimit.DESCRIPTION);
+        footerText.setCharacterLimit(CharacterLimit.FOOTER);
+        authorName.setCharacterLimit(CharacterLimit.AUTHOR);
+        title.setCharacterLimit(CharacterLimit.TITLE);
     }
-    descriptionText.getChildren().add(textNode);
-  }
 
-  /**
-   * Sets the {@link Node#setClip(Node)} values.
-   */
-  private void setClips() {
-    authorAvatarImage.setClip(getHeaderFooterClip());
-    footerImage.setClip(getHeaderFooterClip());
-
-    thumbnailImage.setClip(getRoundedEdgesClip(thumbnailImage));
-    image.setClip(getRoundedEdgesClip(image));
-  }
-
-  /**
-   * Returns the clip for the header and footer image.
-   *
-   * @return The {@link Node#setClip(Node)} for the Header and footer image
-   */
-  private Shape getHeaderFooterClip() {
-    Circle circle = new Circle(10);
-    circle.setLayoutY(10);
-    circle.setLayoutX(10);
-    return circle;
-  }
-
-  /**
-   * Returns a clip that gives the specified node round edges.
-   *
-   * @param imageView The imageView to gift rounded edges to.
-   * @return The {@link Node#setClip(Node)} for it
-   */
-  private Shape getRoundedEdgesClip(ImageView imageView) {
-    Rectangle rectangle = new Rectangle(imageView.getFitWidth(), imageView.getFitHeight());
-    rectangle.setArcHeight(30);
-    rectangle.setArcWidth(30);
-    return rectangle;
-  }
-
-  /**
-   * Returns the length of the full embed text.
-   *
-   * @return The lenght of the whole text
-   */
-  public int getWholeTextLength() {
-    return getChildrenRecursive(rootPane).stream()
-        .filter(node -> node instanceof EditableTextFlow)
-        .map(node -> (EditableTextFlow) node)
-        .map(EditableTextFlow::getText)
-        .map(String::length)
-        .reduce(Math::addExact)
-        .orElse(0);
-  }
-
-  /**
-   * Returns all children and their children and so on.
-   *
-   * @param node The Node to get all children for
-   * @return All children and their children
-   */
-  private List<Node> getChildrenRecursive(Node node) {
-    if (!(node instanceof Parent)) {
-      return Collections.singletonList(node);
+    private void addText(String text, boolean variable) {
+        Text textNode = new Text(text);
+        if (variable) {
+            textNode.getStyleClass().add("variable");
+        }
+        descriptionText.getChildren().add(textNode);
     }
-    List<Node> children = new ArrayList<>();
-    for (Node child : ((Parent) node).getChildrenUnmodifiable()) {
-      children.add(child);
-      children.addAll(getChildrenRecursive(child));
-    }
-    return children;
-  }
 
-  /**
-   * Adds a new dummy field.
-   *
-   * @param inline Whether it is an inline field.
-   */
-  public void addDummyField(boolean inline) {
-    fieldContainer.addNewField("This is the field name", "And the value", inline);
-  }
+    /**
+     * Sets the {@link Node#setClip(Node)} values.
+     */
+    private void setClips() {
+        authorAvatarImage.setClip(getHeaderFooterClip());
+        footerImage.setClip(getHeaderFooterClip());
+
+        thumbnailImage.setClip(getRoundedEdgesClip(thumbnailImage));
+        image.setClip(getRoundedEdgesClip(image));
+    }
+
+    /**
+     * Returns the clip for the header and footer image.
+     *
+     * @return The {@link Node#setClip(Node)} for the Header and footer image
+     */
+    private Shape getHeaderFooterClip() {
+        Circle circle = new Circle(10);
+        circle.setLayoutY(10);
+        circle.setLayoutX(10);
+        return circle;
+    }
+
+    /**
+     * Returns a clip that gives the specified node round edges.
+     *
+     * @param imageView The imageView to gift rounded edges to.
+     * @return The {@link Node#setClip(Node)} for it
+     */
+    private Shape getRoundedEdgesClip(ImageView imageView) {
+        Rectangle rectangle = new Rectangle(imageView.getFitWidth(), imageView.getFitHeight());
+        rectangle.setArcHeight(30);
+        rectangle.setArcWidth(30);
+        return rectangle;
+    }
+
+    /**
+     * Returns the length of the full embed text.
+     *
+     * @return The lenght of the whole text
+     */
+    public int getWholeTextLength() {
+        return getChildrenRecursive(rootPane).stream()
+                .filter(node -> node instanceof EditableTextFlow)
+                .map(node -> (EditableTextFlow) node)
+                .map(EditableTextFlow::getText)
+                .map(String::length)
+                .reduce(Math::addExact)
+                .orElse(0);
+    }
+
+    /**
+     * Returns all children and their children and so on.
+     *
+     * @param node The Node to get all children for
+     * @return All children and their children
+     */
+    private List<Node> getChildrenRecursive(Node node) {
+        if (!(node instanceof Parent)) {
+            return Collections.singletonList(node);
+        }
+        List<Node> children = new ArrayList<>();
+        for (Node child : ((Parent) node).getChildrenUnmodifiable()) {
+            children.add(child);
+            children.addAll(getChildrenRecursive(child));
+        }
+        return children;
+    }
+
+    /**
+     * Adds a new dummy field.
+     *
+     * @param inline Whether it is an inline field.
+     */
+    public void addDummyField(boolean inline) {
+        fieldContainer.addNewField("This is the field name", "And the value", inline);
+    }
 }
--- src/main/java/me/ialistannen/embedcreator/util/Util.java
+++ src/main/java/me/ialistannen/embedcreator/util/Util.java
@@ -1,32 +1,74 @@
 package me.ialistannen.embedcreator.util;
 
-import java.util.function.Supplier;
 import javafx.scene.Node;
 import javafx.scene.control.ContextMenu;
+import me.ialistannen.embedcreator.model.Variable;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
 
 /**
  * Some misc utility methods.
  */
 public class Util {
 
-  /**
-   * Adds a {@link ContextMenu} to a {@link Node}.
-   *
-   * @param node The {@link Node} to attach it to
-   * @param contextMenuSupplier The {@link Supplier} to generate a {@link ContextMenu} to attach
-   */
-  public static void setContextMenu(Node node, Supplier<ContextMenu> contextMenuSupplier) {
-    node.setOnContextMenuRequested(event -> {
-      if (node.getUserData() != null) {
-        return;
-      }
-      node.setUserData("opened");
+    /**
+     * Adds a {@link ContextMenu} to a {@link Node}.
+     *
+     * @param node                The {@link Node} to attach it to
+     * @param contextMenuSupplier The {@link Supplier} to generate a {@link ContextMenu} to attach
+     */
+    public static void setContextMenu(Node node, Supplier<ContextMenu> contextMenuSupplier) {
+        node.setOnContextMenuRequested(event -> {
+            if (node.getUserData() != null) {
+                return;
+            }
+            node.setUserData("opened");
 
-      ContextMenu contextMenu = contextMenuSupplier.get();
-      contextMenu.setAutoHide(true);
-      contextMenu.setOnHidden(hidden -> node.setUserData(null));
+            ContextMenu contextMenu = contextMenuSupplier.get();
+            contextMenu.setAutoHide(true);
+            contextMenu.setOnHidden(hidden -> node.setUserData(null));
 
-      contextMenu.show(node, event.getScreenX(), event.getScreenY());
-    });
-  }
+            contextMenu.show(node, event.getScreenX(), event.getScreenY());
+        });
+    }
+
+    public static List<Variable> getVariables() throws IOException {
+        List<Variable> res = new ArrayList<>();
+        String[] regex = new String[]{"<(\\/)?pre>", "<(\\/)?html>", "<(\\/)?head>", "<(\\/)?body>",
+                "(<pre .+>)\\["};
+        URL url = new URL("https://raw.githubusercontent.com/NegotiumBots/ApparatusWiki/master/variables.md");
+        URLConnection con = url.openConnection(); // (HttpURLConnection)
+        con.setRequestProperty("User-Agent",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:44.0) Gecko/20100101 Firefox/44.0");
+        InputStream is = con.getInputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line;
+        String fin = "";
+        while ((line = br.readLine()) != null) {
+            fin += line;
+        }
+        for (String str : regex) {
+            if (str.startsWith("(pre")) {
+                fin.replaceAll(str, "[");
+            } else {
+                fin.replaceAll(str, "");
+            }
+        }
+
+        String[] split = fin.split(":");
+        for (String str : split) {
+            String[] args = str.replaceAll("[{}]", "").split(";");
+            res.add(new Variable(args[0], Integer.parseInt(args[1])));
+        }
+        return res;
+    }
+
 }
