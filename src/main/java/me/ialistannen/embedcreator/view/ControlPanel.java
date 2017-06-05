369e102004707409904880391d6507f8619155e4
xaanit <shadowjacob@yahoo.com>
2017-06-05 11:50:54 -0500

    Moved generator to new class

--- src/main/java/me/ialistannen/embedcreator/view/ControlPanel.java
+++ src/main/java/me/ialistannen/embedcreator/view/ControlPanel.java
@@ -12,15 +12,13 @@ import javafx.scene.text.Font;
 import me.ialistannen.embedcreator.controller.MainScreenController;
 import me.ialistannen.embedcreator.extraction.Generator;
 import me.ialistannen.embedcreator.extraction.ProviderCollection;
-import me.ialistannen.embedcreator.extraction.ProviderCollection.ProviderType;
-import me.ialistannen.embedcreator.extraction.TextWithUrl;
+import me.ialistannen.embedcreator.util.ApparatusGenerator;
 import me.ialistannen.embedcreator.validation.CharacterLimit;
 import me.ialistannen.embedcreator.validation.ValidationEvent;
 import me.ialistannen.embedcreator.validation.ValidationEvent.ValidationResult;
 import me.ialistannen.embedcreator.variables.VariableRegistry;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
@@ -39,7 +37,7 @@ public class ControlPanel extends BorderPane implements Consumer<ValidationEvent
     private Button generateButton;
 
     private MainScreenController mainScreenController;
-    private Generator generator = new DebugPrintingGenerator();
+    private Generator generator = new ApparatusGenerator();
     private ProviderCollection providerCollection;
 
     /**
@@ -175,109 +173,5 @@ public class ControlPanel extends BorderPane implements Consumer<ValidationEvent
         }
 
         return output.toString();
-    }
-
-    private static class DebugPrintingGenerator implements Generator {
-
-        @Override
-        public String generate(ProviderCollection data) {
-            String res = "";
-            for (ProviderType providerType : ProviderType.values()) {
-                switch (providerType.name().toUpperCase()) {
-                    case "FIELDS": {
-                        res += parseEmbed(data.get(providerType)) + " ";
-                        break;
-                    }
-                    case "COLORED_BAR": {
-                        if (!data.get(providerType).isEmpty()) {
-                            String hex = Integer.toHexString(Integer.parseInt(data.get(providerType).get(0).toString().replace("0x", ""), 16));
-                            res += "{Color:#" + hex.substring(0, hex.length() - 2) + "} ";
-                        }
-                        break;
-                    }
-                    case "AUTHOR_TEXT": {
-                        if (!data.get(providerType).isEmpty()) {
-                            List<TextWithUrl> list = data.get(providerType);
-                            TextWithUrl author = list.get(0);
-                            res += "{AuthorName:" + author.getText() + "} ";
-                            if (author.getUrl() != null) {
-                                res += "{AuthorURL:" + author.getUrl() + "} ";
-                            }
-                        }
-                        break;
-                    }
-
-                    case "AUTHOR_IMAGE": {
-                        if (!data.get(providerType).isEmpty()) {
-                            if (!data.get(providerType).get(0).toString().isEmpty()) {
-                                res += "{AuthorIcon:" + data.get(providerType).get(0).toString() + "} ";
-                            }
-                        }
-                        break;
-                    }
-
-                    case "THUMBNAIL_IMAGE": {
-                        if (!data.get(providerType).isEmpty()) {
-                            if (!data.get(providerType).get(0).toString().isEmpty()) {
-                                res += "{Thumbnail:" + data.get(providerType).get(0).toString() + "} ";
-                            }
-                        }
-                        break;
-                    }
-
-                    case "TITLE": {
-                        if (!data.get(providerType).isEmpty()) {
-                            List<TextWithUrl> list = data.get(providerType);
-                            TextWithUrl title = list.get(0);
-                            res += "{Title:" + title.getText() + "} ";
-                            if (title.getUrl() != null)
-                                res += "{TitleURL:" + title.getUrl() + "} ";
-                        }
-                        break;
-                    }
-
-                    case "DESCRIPTION": {
-                        if (!data.get(providerType).isEmpty()) {
-                            res += "{Description:" + data.get(providerType).get(0).toString() + "} ";
-                        }
-                        break;
-                    }
-
-                    case "IMAGE": {
-                        if (!data.get(providerType).isEmpty()) {
-                            if (!data.get(providerType).get(0).toString().isEmpty()) {
-                                res += "{Image:" + data.get(providerType).get(0).toString() + "} ";
-                            }
-                        }
-                        break;
-                    }
-                    case "FOOTER_IMAGE": {
-                        if (!data.get(providerType).isEmpty()) {
-                            if (!data.get(providerType).get(0).toString().isEmpty()) {
-                                res += "{FooterIcon:" + data.get(providerType).get(0).toString() + "} ";
-                            }
-                        }
-                        break;
-                    }
-                    case "FOOTER_TEXT": {
-                        if (!data.get(providerType).isEmpty()) {
-                            res += "{FooterText:" + data.get(providerType).get(0).toString() + "} ";
-                        }
-                        break;
-                    }
-                }
-            }
-            return res.trim();
-        }
-
-        private String parseEmbed(List<EmbedField> embeds) {
-            String res = "";
-            for (EmbedField em : embeds) {
-                res += "{FieldTitle:" + em.getName() + " FieldText:" + em.getValue() + " Inline:" + em.isInline() + "} ";
-            }
-            return res.replaceAll("", "").trim();
-        }
-
-
     }
 }
